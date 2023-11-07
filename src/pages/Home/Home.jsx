bda828ddee7a94a8efc9c813ff95e33ed876a0ae
Md Badrul Alam <developer.badrul@gmail.com>
2023-11-07 22:37:27 +0600

    add tow Slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Service from "../Services/Service";
-import { Spinner } from "flowbite-react";
+import { Button, Spinner } from "flowbite-react";
+import { Link } from "react-router-dom";
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
     const [loadServices, setLoadServices] = useState(null);
@@ -22,10 +24,16 @@ const Home = () => {
             });
     }, []);
 
-    console.log("load service Home Page", loadServices);
+    // console.log("load service Home Page", loadServices);
 
     return (
         <div>
+            {/* banner section  */}
+            <div className="w-11/12 mx-auto my-2">
+            <Banner></Banner>
+            </div>
+
+            {/* Popular Service Section  */}
             {loading ? (
                 <div className="text-center">
                     <Spinner color="success" aria-label="Success spinner example" size="xl" className="h-screen" />
@@ -38,11 +46,12 @@ const Home = () => {
                 <div>
                     <div>
                         <h1 className="text-center text-5xl text-emerald-600 font-bold py-4">Popular Services</h1>
-                        <div className="grid md:grid-cols-2 w-10/12 mx-auto">
+                        <div className="grid md:grid-cols-2 gap-5 w-10/12 mx-auto">
                             {loadServices?.slice(0, 4)?.map((service) => (
                                 <Service key={service._id} service={service}></Service>
                             ))}
                         </div>
+                        <Link className="w-10/12 block mx-auto my-3" to="/services"><Button gradientMonochrome="success" className="font-bold mx-auto">See All Services</Button></Link>
                     </div>
                 </div>
             )}
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,6 +1,5 @@
 import { useLoaderData } from "react-router-dom";
 import Service from "./Service";
-import { Button } from "flowbite-react";
 
 const Services = () => {
     const allServices = useLoaderData();
@@ -8,12 +7,11 @@ const Services = () => {
     return (
         <div>
             <h2 className="text-center text-5xl text-emerald-600 font-bold py-4">Total Services:{allServices.length}</h2>
-            <div className="grid md:grid-cols-2 gap-5 md:w-11/12 mx-auto py-5">
+            <div className="grid md:grid-cols-3 gap-5 md:w-11/12 mx-auto py-5">
                 {
                     allServices.map(service => <Service key={service._id} service={service}></Service>)
                 }
             </div>
-            <Button gradientMonochrome="info">Info</Button>
         </div>
     );
 };

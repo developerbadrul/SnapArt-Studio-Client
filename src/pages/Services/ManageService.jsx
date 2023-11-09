3cc8a12fc912e0715e1681a3a2e4387df0d24778
Md Badrul Alam <developer.badrul@gmail.com>
2023-11-09 17:10:01 +0600

    try to add delete req

--- src/pages/Services/ManageService.jsx
+++ src/pages/Services/ManageService.jsx
@@ -5,7 +5,7 @@ import useAuth from "../../hook/useAuth";
 import PageTitle from "../../components/PageTitle/PageTitle";
 
 const ManageService = () => {
-    const {loggedUser} = useAuth();
+    const { loggedUser } = useAuth();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [orders, setOrders] = useState([]);
@@ -24,8 +24,21 @@ const ManageService = () => {
             });
     }, [clientEmail]);
 
+    const handleDeleteOrder = (orderId) => {
+        // axios.delete(`https://snapart-server.vercel.app/orders/${orderId}`)
+        axios.delete(`localhost/orders/${orderId}`)
+            .then(() => {
+                // Update the state to remove the deleted order
+                setOrders((prevOrders) => prevOrders.filter((order) => order._id !== orderId));
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     return (
         <div>
+            <h1 className="text-5xl text-green-600 font-bold text-center my-5">Your Order List</h1>
             {loading ? (
                 <div className="text-center">
                     <Spinner color="success" aria-label="Loading spinner" size="xl" className="h-screen" />
@@ -33,35 +46,49 @@ const ManageService = () => {
             ) : error ? (
                 <div>Error: {error.message}</div>
             ) : (
-                <Table striped>
+                <div>
                     <PageTitle title={"SnapArt || Manage Service"}></PageTitle>
-                    <Table.Head>
-                        <Table.HeadCell>Service Name</Table.HeadCell>
-                        <Table.HeadCell>Service Provider</Table.HeadCell>
-                        <Table.HeadCell>Order Date</Table.HeadCell>
-                        <Table.HeadCell>Service Price</Table.HeadCell>
-                        <Table.HeadCell>
-                            <span className="sr-only">Edit</span>
-                        </Table.HeadCell>
-                    </Table.Head>
-                    <Table.Body className="divide-y">
-                        {orders.map(order => (
-                            <Table.Row key={order._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark-text-white">
-                                    {order.serviceName}
-                                </Table.Cell>
-                                <Table.Cell>{order.serviceProviderName}</Table.Cell>
-                                <Table.Cell>{order.OrderDate}</Table.Cell>
-                                <Table.Cell>${order.ServicePrice}</Table.Cell>
-                                <Table.Cell>
-                                    <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                                        Edit
-                                    </a>
-                                </Table.Cell>
-                            </Table.Row>
-                        ))}
-                    </Table.Body>
-                </Table>
+                    {orders.length === 0 ? (
+                        <p className="text-center text-2xl text-red-600 font-bold py-4">
+                            You have no orders
+                        </p>
+                    ) : (
+                        <Table striped>
+                            <Table.Head>
+                                <Table.HeadCell>Service Name</Table.HeadCell>
+                                <Table.HeadCell>Service Provider</Table.HeadCell>
+                                <Table.HeadCell>Order Date</Table.HeadCell>
+                                <Table.HeadCell>Service Price</Table.HeadCell>
+                                <Table.HeadCell>
+                                    <span className="sr-only">Actions</span>
+                                </Table.HeadCell>
+                            </Table.Head>
+                            <Table.Body className="divide-y">
+                                {orders.map((order) => (
+                                    <Table.Row key={order._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark-text-white">
+                                            {order.serviceName}
+                                        </Table.Cell>
+                                        <Table.Cell>{order.serviceProviderName}</Table.Cell>
+                                        <Table.Cell>{order.OrderDate}</Table.Cell>
+                                        <Table.Cell>${order.ServicePrice}</Table.Cell>
+                                        <Table.Cell className="mx-3">
+                                            <a href="#" className="block font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                                                Edit
+                                            </a>
+                                            <button
+                                                className="block btn btn-error"
+                                                onClick={() => handleDeleteOrder(order._id)}
+                                            >
+                                                Delete
+                                            </button>
+                                        </Table.Cell>
+                                    </Table.Row>
+                                ))}
+                            </Table.Body>
+                        </Table>
+                    )}
+                </div>
             )}
         </div>
     );
